a5322593df354221bafb4b2cbc47185ac61f95f5
Junrong Fu <junrongfu6@gmail.com>
2023-01-26 20:59:51 +0800

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { logout } = useLogout();
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -9,6 +11,11 @@ const Navbar = () => {
         <li>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
